5eeb8fb2ef518003fabebc1eb7f3137557bd2681
zhanchihu <zhanchihu@gmail.com>
2024-07-18 14:33:28 +0000

    fix utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,8 +2,6 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { Active, DataRef, Over } from "@dnd-kit/core";
 
-type DraggableData = ColumnDragData | TaskDragData;
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }

e3948781171755f09ab0a5e409167a7955cacd01
zhanchihu <zhanchihu@gmail.com>
2024-07-22 11:36:58 +0000

    fix css style

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,15 +1,11 @@
 import React, { CSSProperties } from 'react';
 
-const logoStyle: CSSProperties = {
-  '--logo-color': '#FFFFFF' as string,
-};
-
 const Logo = () => (
     <img
        src="/logo.svg" 
        alt="OArmour Logo" 
        className="w-20 h-20 mr-4" 
-      style={logoStyle}
+       style={{ '--logo-color': '#FFFFFF' }}
     />
 );
 

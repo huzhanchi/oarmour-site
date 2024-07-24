0717033ffbb7cecc626fb3efd55d3813f1dfe6f4
huzhanchi <zhanchihu@gmail.com>
2024-07-24 07:11:39 +0000

    fix css var

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,9 +4,6 @@ export default function Logo() {
       src="/logo.svg" 
       alt="OArmour Logo" 
       className="w-16 h-16 mr-4 dark:invert" 
-      style={{ 
-        '--logoColor': '#FFFFFF',
-      }}
     />
   </div>
 }

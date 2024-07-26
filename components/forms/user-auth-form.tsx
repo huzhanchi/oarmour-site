2eccf73dfd677deca2b526da7235dddc3936c455
huzhanchi <zhanchihu@gmail.com>
2024-07-26 05:18:19 +0000

    keep github login

--- components/forms/user-auth-form.tsx
+++ components/forms/user-auth-form.tsx
@@ -15,7 +15,7 @@ import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import GoogleSignInButton from "../github-auth-button";
+import GithubSignInButton from "../github-auth-button";
 
 const formSchema = z.object({
   email: z.string().email({ message: "Enter a valid email address" }),
@@ -44,7 +44,7 @@ export default function UserAuthForm() {
 
   return (
     <>
-      <Form {...form}>
+      {/* <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-2 w-full"
@@ -82,8 +82,8 @@ export default function UserAuthForm() {
             Or continue with
           </span>
         </div>
-      </div>
-      <GoogleSignInButton />
+      </div> */}
+      <GithubSignInButton />
     </>
   );
 }
